a5a269aa61476693d1971ce6a5324ef313c5142d
Adam Matthews <adam.42@hotmail.co.uk>
2020-04-30 22:43:18 +0100

    Spider now moves towards player

--- Assets/Scripts/SpiderController.cs
+++ Assets/Scripts/SpiderController.cs
@@ -5,31 +5,63 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class SpiderController : MonoBehaviour {
 
+    public enum State {
+        IDLE,
+        WALK
+    }
+
     public Animator animator;
 
-    public float maxWalkSpeed = 1f;
+    public GameObject targetBody;
+    public float maxDistanceFromTarget;
 
-    public bool test = true;
+    public float maxWalkSpeed;
+    public float walkSpeedIncreaseRate;
+
+    public float maxRotationSpeed;
+    
+    private float currentSpeedModifier = 0f;
+
+    private float sqrMaxDistanceFromTarget;
+
+    private State state = State.IDLE;
 
     // Start is called before the first frame update
     void Start() {
         animator = GetComponent<Animator>();
 
-        StartCoroutine(testFunc());
+        sqrMaxDistanceFromTarget = maxDistanceFromTarget * maxDistanceFromTarget;
     }
 
     // Update is called once per frame
     void Update() {
-        if(test) {
-            return;
-        }
-        transform.position -= new Vector3(maxWalkSpeed * Time.deltaTime, 0, 0);
-
-        animator.SetFloat("Speed", 1);
     }
 
-    IEnumerator testFunc() {
-        yield return new WaitForSeconds(5);
-        test = false;
+    private void FixedUpdate() {
+
+        Vector3 dirTowardTargetObject = targetBody.transform.position - transform.position;
+
+        if (dirTowardTargetObject.sqrMagnitude <= sqrMaxDistanceFromTarget) {
+            state = State.IDLE;
+            currentSpeedModifier = 0;
+        } else {
+            state = State.WALK;
+
+            currentSpeedModifier = Mathf.Clamp(currentSpeedModifier + (Time.deltaTime * walkSpeedIncreaseRate), 0, 1);
+
+            float speed = currentSpeedModifier * maxWalkSpeed;
+
+            dirTowardTargetObject.Normalize();
+            dirTowardTargetObject.y = transform.position.y;
+
+            transform.position += (dirTowardTargetObject * speed * Time.deltaTime);
+        }
+
+        animator.SetFloat("Speed", currentSpeedModifier);
+
+        Vector3 rotation = dirTowardTargetObject;
+        rotation.y = 0;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(rotation), maxRotationSpeed * Time.deltaTime);
     }
 }
